fb56be46d672da2e6db5b409f13ec3d2e834f0fb
Koki_Zingy <95992779+KokiZingy@users.noreply.github.com>
2022-02-20 09:36:19 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+//discord.jsを使用
+const { Client, Intents, MessageEmbed } = require('discord.js');
+//クライアントを作成
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });  
+//コマンドのPREFIXを指定
+const prefix = '!'
+
+//起動時に実行
+client.on('ready', () => {
+  console.log('起動完了！。');  
+});
+
+//メッセージ送信時に実行
+client.on('messageCreate', message => {
+  if (message.content === 'こんにちは') {
+    message.channel.send('こんにちは～')
+  } else if (message.content === `${prefix}help`) {
+    const embed = new MessageEmbed()
+     .setTitle('help')
+     .setDescription('コマンド一覧を掻くといいです')
+    message.channel.send({embeds: [embed]})
+  }
+});
+
+//Botにログイン
+client.login(process.env.token);
